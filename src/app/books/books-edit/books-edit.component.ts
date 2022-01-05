7fa5e3b43e36a9e7801bbe7daacde3d31af634ce
Henrik Vesterinen <henrikv@iki.fi>
2022-01-05 17:00:09 +0200

    fix problems in reactive form

--- src/app/books/books-edit/books-edit.component.ts
+++ src/app/books/books-edit/books-edit.component.ts
@@ -1,6 +1,14 @@
-import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Inject,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Author } from 'src/app/shared/models/author.model';
 import { Book } from 'src/app/shared/models/book.model';
 
 export type DialogData = { book: Partial<Book>; saved: EventEmitter<any> };
@@ -17,38 +25,51 @@ export class BooksEditComponent implements OnInit {
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
-    private builder: FormBuilder
+    private builder: FormBuilder,
+    private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
-    const authorControls = this.data.book.authors?.map((author) =>
-      this.builder.group({
-        firstNames: author.firstNames,
-        lastName: author.lastName,
-      })
+    const authorGroups = this.data.book.authors?.map((author) =>
+      this.newAuthorGroup(author)
     );
     this.bookForm = this.builder.group({
       id: { value: this.data.book.id, disabled: true },
       title: this.data.book.title,
       yearPublished: this.data.book.yearPublished,
-      authors: this.builder.array(authorControls || []),
+      authors: this.builder.array(authorGroups || []),
     });
 
-    console.log('author groups', authorControls);
+    console.log('author groups', authorGroups);
   }
 
   get authors(): FormArray {
     return this.bookForm.get('authors') as FormArray;
   }
 
-  // newAuthor(): FormGroup {
-  //   return this.fb.group({
-  //     firstNames: '',
-  //     lastName: '',
-  //   });
-  // }
+  newAuthorGroup(author?: Author | undefined): FormGroup {
+    return this.builder.group({
+      firstNames: author?.firstNames || '',
+      lastName: author?.lastName || '',
+    });
+  }
 
-  // addAuthor() {
-  //   this.authors().push(this.newAuthor());
-  // }
+  onSubmit(book: Book) {
+    console.log('trying to submit', book);
+  }
+
+  addAuthor() {
+    this.authors.push(this.newAuthorGroup());
+    /*
+    Now this will give an error:
+    *Error: NG0100: ExpressionChangedAfterItHasBeenCheckedError: 
+    Expression has changed after it was checked. 
+    Previous value: 'false'. Current value: 'true'
+
+    See
+    https://stackoverflow.com/questions/34364880/expression-has-changed-after-it-was-checked
+    Error can be fixed with this:
+    */
+    this.cdr.detectChanges(); // WTF is this?
+  }
 }
